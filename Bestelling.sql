28fa6afaf1244abf86945ae18c845a73c5aec7e7
zanerebouded@gmail.com <TheUnquiet>
2024-05-20 21:52:11 +0200

    Added files to Sql folder.

--- Bestelling.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[Bestelling]
-(
-	[offerte_id] INT NOT NULL,
-	[product_id] INT NOT NULL,
-	[aantal] INT NOT NULL,
-	CONSTRAINT [FK_Bestelling_Offerte] FOREIGN KEY ([offerte_id]) REFERENCES [Offerte]([id]),
-	CONSTRAINT [FK_Bestelling_Product] FOREIGN KEY ([product_id]) REFERENCES Product([id]),
-	PRIMARY KEY ([offerte_id], [product_id])
-)
--- Sql/aantal_offertes_klant.sql
+++
@@ -1,4 +0,0 @@
-select k.naam, count(o.id) as 'aantal'
-from offerte o
-join klant k on k.id = o.klantnummer
-group by k.naam;
